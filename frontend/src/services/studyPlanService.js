9a70f2cb6fdc1f641a216e39958c3691a46a067c
KILLERNEGAN <kisarasirimanne21@gmail.com>
2026-04-01 22:20:53 +0530

    study planner continue

--- frontend/src/services/studyPlanService.js
+++ frontend/src/services/studyPlanService.js
@@ -13,29 +13,29 @@ const getAuthConfig = () => {
 
 // ── Plans ──────────────────────────────────────────────
 export const createPlan = (data) =>
-  axios.post(API, data, getAuthConfig());
+  axios.post(API, data, getAuthConfig()).then(res => res.data);
 
 export const getPlans = () =>
-  axios.get(API, getAuthConfig());
+  axios.get(API, getAuthConfig()).then(res => res.data);
 
 export const getPlan = (id) =>
-  axios.get(`${API}/${id}`, getAuthConfig());
+  axios.get(`${API}/${id}`, getAuthConfig()).then(res => res.data);
 
 export const updatePlan = (id, data) =>
-  axios.put(`${API}/${id}`, data, getAuthConfig());
+  axios.put(`${API}/${id}`, data, getAuthConfig()).then(res => res.data);
 
 export const deletePlan = (id) =>
-  axios.delete(`${API}/${id}`, getAuthConfig());
+  axios.delete(`${API}/${id}`, getAuthConfig()).then(res => res.data);
 
 // ── Tasks ──────────────────────────────────────────────
 export const addTask = (planId, data) =>
-  axios.post(`${API}/${planId}/tasks`, data, getAuthConfig());
+  axios.post(`${API}/${planId}/tasks`, data, getAuthConfig()).then(res => res.data);
 
 export const getTasks = (planId) =>
-  axios.get(`${API}/${planId}/tasks`, getAuthConfig());
+  axios.get(`${API}/${planId}/tasks`, getAuthConfig()).then(res => res.data);
 
 export const updateTask = (planId, taskId, data) =>
-  axios.put(`${API}/${planId}/tasks/${taskId}`, data, getAuthConfig());
+  axios.put(`${API}/${planId}/tasks/${taskId}`, data, getAuthConfig()).then(res => res.data);
 
 export const deleteTask = (planId, taskId) =>
-  axios.delete(`${API}/${planId}/tasks/${taskId}`, getAuthConfig());
+  axios.delete(`${API}/${planId}/tasks/${taskId}`, getAuthConfig()).then(res => res.data);
